4346cb9dbcaf53b46c893361071ddd9df8723cb6
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-10-16 08:34:38 -0400

    added path to config dir for server

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+require('dotenv').config({path: './config/.env'})
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.render('main.ejs')
