162cf2873af11b2184ce7262e04e691449dab3d0
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-10-17 20:37:42 -0400

    added addsignouts to main controller

--- controllers/main.js
+++ controllers/main.js
@@ -3,7 +3,21 @@ const Signouts = require('../models/Signouts')
 module.exports = {
   getMain: async (req, res) => {
     try{
-      res.render('main.ejs')
+      const signouts = await Signouts.find()
+      res.render('main.ejs', { signouts: signouts })
+    }catch(err) {
+      console.log(err)
+    }
+  },
+  addSignouts: async (req, res) => {
+    try{
+      await Signouts.create({
+        name: req.name,
+        msa: req.msa,
+        key: req.key,
+
+      })
+      res.redirect('/')
     }catch(err) {
       console.log(err)
     }
