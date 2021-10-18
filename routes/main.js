e53f6e3a1b9b5481deb49c944bd01fe91eff2881
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-10-17 20:38:32 -0400

    added route for adding to signouts

--- routes/main.js
+++ routes/main.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const mainController = require('../controllers/main')
 
 router.get('/', mainController.getMain)
+router.get('/add', mainController.addSignouts)
 
 module.exports = router
